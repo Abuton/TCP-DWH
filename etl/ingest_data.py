857191cb54846b726bc1f7183e434a26c8542d23
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-22 19:30:02 +0100

    added logging & refactor code structure

--- etl/ingest_data.py
+++ etl/ingest_data.py
@@ -1,5 +1,10 @@
 from pyspark.sql import SparkSession
+import logging
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s - Ingest Data - %(message)s", level=logging.INFO
+)
+logging.getLogger().setLevel(logging.INFO)
 tpcdsRootDir = "datasapien_dwh"
 tpcdsWorkDir = "datasapien_dwh/work"
 tpcdsDdlDir = "../sql_ddl_codes"
@@ -7,15 +12,15 @@ tpcdsGenDataDir = "../src/data"
 tpcdsQueriesDir = "../src/queries"
 tpcdsDatabaseName = "TPCDS1G"
 
-print("TPCDS root directory is at : ", tpcdsRootDir)
-print("TPCDS ddl scripts directory is at: ", tpcdsDdlDir)
-print("TPCDS data directory is at: ", tpcdsGenDataDir)
-print("TPCDS queries directory is at: ", tpcdsQueriesDir)
+logging.info("TPCDS root directory is at : ", tpcdsRootDir)
+logging.info("TPCDS ddl scripts directory is at: ", tpcdsDdlDir)
+logging.info("TPCDS data directory is at: ", tpcdsGenDataDir)
+logging.info("TPCDS queries directory is at: ", tpcdsQueriesDir)
 
 
 def get_spark_session():
     spark = SparkSession.builder.\
-        config("spark.ui.showConsoleProgress", True).\
+        config("spark.ui.showConsoleProgress", False).\
         config("spark.sql.autoBroadcastJoinThreshold", -1).\
         config("spark.sql.crossJoin.enabled", True).\
         getOrCreate()
@@ -26,18 +31,17 @@ def get_spark_session():
 
 def clearTableDirectory(tableName):
     import os
-    if os.path.isdir(f"spark-warehouse/tpcds1g.db/{tableName}"):
-        os.remove(f"spark-warehouse/tpcds1g.db/{tableName}/*")
+    if os.path.isdir(f"spark-warehouse/tpcds1g.db/{tableName}/"):
         os.remove(f"spark-warehouse/tpcds1g.db/{tableName}")
     else:
-        print("Creating path for DWH")
+        logging.info(f"Creating path for DWH for table {tableName}")
 
 
 def createDatabase(spark):
     spark.sql(f"DROP DATABASE IF EXISTS {tpcdsDatabaseName} CASCADE")
-    spark.sql(f"CREATE DATABASE {tpcdsDatabaseName}")
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS {tpcdsDatabaseName}")
     spark.sql(f"USE {tpcdsDatabaseName}")
-    print(f"{tpcdsDatabaseName} Created Successfully")
+    logging.info(f"{tpcdsDatabaseName} Created Successfully")
 
 
 #  Function to create a table in spark. It reads the DDL script for each of the
@@ -45,7 +49,7 @@ def createDatabase(spark):
 
 def createTable(tableName: str, spark):
     clearTableDirectory(tableName)
-    print(f"Creating table {tableName} ..")
+    logging.info(f"Creating table {tableName} ..")
     spark.sql(f"DROP TABLE IF EXISTS {tableName}")
     _, content = spark.sparkContext.wholeTextFiles(f"{tpcdsDdlDir}/{tableName}.sql").collect()[0]
 
@@ -57,7 +61,7 @@ def createTable(tableName: str, spark):
         try:
             spark.sql(stmt.strip())
         except Exception as e:
-            print("Error Occurred", e)
+            logging.info(f"Error Occurred {e}")
 
 
 if __name__ == "__main__":
