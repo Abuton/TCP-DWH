a1c9b2960425025341871a46e0fd929e2ad33b97
Abuton <alaroabubakarolayemi@yahoo.com>
2021-12-23 01:44:02 +0100

    added logging & refactor code structure

--- etl/pk_integrity.py
+++ etl/pk_integrity.py
@@ -1,33 +1,38 @@
-from pyspark.sql import SparkSession
+from ingest_data import get_spark_session, createDatabase
+import logging
+from pprint import pprint
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s - PK Consistency Check - %(message)s",
+    level=logging.INFO,
+)
+logging.getLogger().setLevel(logging.INFO)
 data_path = "spark-warehouse/tpcds1g.db"
-
-
-def get_spark_session():
-    spark = SparkSession.builder.\
-        config("spark.ui.showConsoleProgress", True).\
-        config("spark.sql.autoBroadcastJoinThreshold", -1).\
-        config("spark.sql.crossJoin.enabled", True).\
-        getOrCreate()
-
-    spark.sparkContext.setLogLevel("ERROR")
-    return spark
 
 
 def read_table(tableName: str, spark):
     df = spark.read.parquet(f"{data_path}/{tableName}")
+    pprint(df.printSchema())
     return df
 
 
-def check_pk_integrity(tableName: str, pk_column: str, spark):
-    df = read_table(tableName, spark)
-    column = df.select(pk_column)
-    if column.isNull():
-        print()
-    else:
-        print()
+def add_pk_constraint(spark, tableName: str, pkColumn: str):
+    stmt = f"alter table {tableName} add primary key ({pkColumn});"
+    try:
+        spark.sql(stmt)
+    except Exception as e:
+        print("An Error Occurred", e)
 
 
 if __name__ == "__main__":
     spark = get_spark_session()
-    df = read_table("store", spark)
-    df.show(3)
+    createDatabase(spark)
+    tables_pk_column = {
+        "customer": "c_customer_sk",
+        "item": "i_item_sk",
+        "store": "s_store_sk",
+        "date_dim": "d_date_sk",
+        "store_sales": "ss_sold_date_sk",
+    }
+    for table in tables_pk_column.keys():
+        add_pk_constraint(spark, table, tables_pk_column[table])
